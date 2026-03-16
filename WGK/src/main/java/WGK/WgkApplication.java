eb4c2e146c0429b5e15ee55857bcc1731db17b35
IanBrenes <brenes.ian.cr7@gmail.com>
2026-03-15 19:26:58 -0600

    Se hace la carga inicial del proyecto

---
+++ WGK/src/main/java/WGK/WgkApplication.java
@@ -0,0 +1,13 @@
+package WGK;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WgkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WgkApplication.class, args);
+	}
+
+}
---
+++ WGK/src/test/java/WGK/WgkApplicationTests.java
@@ -0,0 +1,13 @@
+package WGK;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WgkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
